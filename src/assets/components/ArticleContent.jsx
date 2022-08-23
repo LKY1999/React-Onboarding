3e6bd2adcdb3ea51755fbf769e2e4d4af7b1bd4b
Kai Ying <kaiylim99@gmail.com>
2022-08-23 18:53:59 +0800

    styling for inner page comment section

--- src/assets/components/ArticleContent.jsx
+++ src/assets/components/ArticleContent.jsx
@@ -3,10 +3,9 @@ import CommentCard from "./CommentCard";
 import { Link } from "react-router-dom";
 
 export default function Article({ news, id }) {
-  console.log(news.comments);
   if (news.id == id) {
     return (
-      <div className="py-20 px-20">
+      <div className="py-20 px-20 w-9/12 m-auto">
         <div className="flex items-end">
           <Link to={"/"}>
             <img src="/src/assets/back.svg" alt="back" className="h-5 inline" />
@@ -17,12 +16,12 @@ export default function Article({ news, id }) {
           {news.title}
         </h1>
 
-        <img className="" src={news.imageUrl} alt={news.imageAlt} />
+        <img className=" mb-10" src={news.imageUrl} alt={news.imageAlt} />
         <div>
           <p>{news.content}</p>
         </div>
 
-        <div>
+        <div className="border-y-8">
           <h2 className="py-10 text-3xl">Comments</h2>
           <form className=" border-2 rounded max-w-sm">
             <input
@@ -32,10 +31,11 @@ export default function Article({ news, id }) {
               value={"Email"}
             ></input>
           </form>
-
-          {news.comments.map((comment) => (
-            <CommentCard comment={comment} key={news.comments.email} />
-          ))}
+          <div className=" mx-8">
+            {news.comments.map((comment) => (
+              <CommentCard comment={comment} key={news.comments.email} />
+            ))}
+          </div>
         </div>
       </div>
     );
--- src/assets/components/CommentCard.jsx
+++ src/assets/components/CommentCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function CommentCard({ comment }) {
   return (
-    <div className="rounded-lg bg-white shadow-lg overflow-hidden">
+    <div className="border-b-2 py-8 last:border-b-0">
       <div className="px-6 py-4">
         <h2 className="text-lg font-semibold text-gray-800">{comment.email}</h2>
         <p className="text-gray-600">{comment.content}</p>
